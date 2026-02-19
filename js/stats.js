6bde06a5e51e609e95d7c7eeae811cc2fd266e49
roxdbs <roxane.dubus4@gmail.com>
2026-02-19 13:28:02 +0100

    Ajout de retours visuels sur les comportements

--- js/stats.js
+++ js/stats.js
@@ -1,7 +1,12 @@
 // Calcul des proportions d'intervalles de BEH2 et BEH3
 export function calculateProportion(array) {
-    if (array.length === 0) return 0;
+    if (array.length === 0) return { trueCount: 0, prop: 0, percent: 0 };
 
     const trueCount = array.filter(value => value === true).length;
-    return trueCount / array.length;
+    const prop = trueCount / array.length;
+    return {
+        trueCount,
+        prop,
+        percent: Math.round(prop * 100)
+    };
 }
